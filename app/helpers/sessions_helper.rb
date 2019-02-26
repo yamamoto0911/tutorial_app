6c2b3eb61f22ce79881f26fd15d23982014656a9
Yamamoto Yuya <chuo.yamamoto0911@gmail.com>
2019-02-26 22:04:46 +0000

    Finish user edit index destroy

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,10 @@ module SessionsHelper
     end
   end
   
+  def current_user?(user)
+    user == current_user
+  end
+  
   def logged_in?
     !current_user.nil?
   end
@@ -19,4 +23,16 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  
+  #記憶しているURLにリダイレクトする
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  #アクセスしようとしたURLを記憶しておく  
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
 end
