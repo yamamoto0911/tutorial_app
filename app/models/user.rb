6b862d433b1e2e1cf6be1dd8fa9c9105e5757f66
Yamamoto Yuya <chuo.yamamoto0911@gmail.com>
2019-03-04 11:31:36 +0000

    finish basic informaition

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX},
+                    format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :department, length: { in: 3..50 }, allow_blank: true
 end
-
