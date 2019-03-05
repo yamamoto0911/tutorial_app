58180b74a2952d950414bc1420364e39be953e8c
Yamamoto Yuya <chuo.yamamoto0911@gmail.com>
2019-03-05 22:26:58 +0000

    finish attendances show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
       end
     end
     @dates = @user.attendances.where('worked_on >= ? and worked_on <= ?', @first_day, @last_day).order('worked_on')
+    @worked_sum = @dates.where.not(started_at: nil).count
   end
 
   def new
--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -6,6 +6,13 @@ module AttendancesHelper
       Time.now.day,
       Time.now.hour,
       Time.now.min, 0)
-      
+  end
+  
+  def working_times(started_at, finished_at)
+    format("%.2f", (((finished_at - started_at) / 60) / 60.0))
+  end
+  
+  def working_times_sum(seconds)
+    format("%.2f", seconds / 60 / 60.0)
   end
 end
